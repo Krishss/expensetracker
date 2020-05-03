09125b7c43a21ee58b7f04b145698d408cc37c8d
Krishnan Sundaresan <krishsks@krishnans-mbp.myfiosgateway.com>
2020-05-03 15:38:47 -0400

    Initial commit

---
+++ lib/models/global.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//contains text & fonts available to this project;
+
+Color teelcolor = new Color(0xFFA0D36F);
+
+TextStyle headerTitleStyle = new TextStyle(fontFamily: 'Avenir', fontWeight: FontWeight.bold);
