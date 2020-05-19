c715291d09884a299d77603cc06b0db8ee56af95
krishSKS <krishsodikumar@gmail.com>
2020-05-19 19:48:47 -0400

    Adding splash screen & Moving homePage to different page

---
+++ lib/HomePage.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+//import 'models/global.dart';
+//import 'package:trie/trie.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trackenser',
+      theme: ThemeData(
+        //primarySwatch: Colors.cyan,
+          fontFamily: 'Raleway'
+      ),
+      home: MyHomePage(title: 'Expense Tracker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedTabIndex = 0;
+
+  List _pages = [
+    Text("Home"),
+    //Text("Search"),
+    Text("Trip"),
+    Text("Trip Event Details"),
+  ];
+
+  _changeIndex(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    //Screen UI assembly
+    return Scaffold(
+      appBar: PreferredSize( //add this to actually increase size of top bar and place appbar
+        preferredSize: Size.fromHeight(120.0),
+
+        child: AppBar(title: Text(widget.title, style: TextStyle(fontFamily: 'open_sans'),),
+          //titleSpacing: ,
+          centerTitle: true,
+          backgroundColor: Colors.lime[900],
+          shape: RoundedRectangleBorder( borderRadius:
+          BorderRadius.vertical(bottom: Radius.circular(30)
+          )
+          ),
+
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // do something`
+              },
+            )
+          ],),
+      ),
+      //Container(height: 100,color: Colors.cyan,)
+
+
+      body: ListView(
+        children: <Widget>[
+          Card( child: ListTile(
+            leading: Icon(Icons.radio_button_unchecked),
+            title: Text('Trip location A'),
+          )),
+          Card( child: ListTile(
+            leading: Icon(Icons.radio_button_unchecked),
+            title: Text('Trip location B'),
+            subtitle: Text('Bachelor Party !'),
+            trailing: Icon(Icons.more_vert),
+          ))
+        ],
+      ),
+      //Center(child: _pages[_selectedTabIndex],),
+
+      floatingActionButton: new FloatingActionButton(
+        onPressed: (){} ,//
+        backgroundColor: Colors.lime[800],
+        splashColor: Colors.lime,
+        //tooltip: 'Increment',
+        child: new Icon(Icons.add),
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTabIndex,
+        onTap: _changeIndex,
+        selectedItemColor: Colors.teal,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.radio), title: Text("Home")),
+          //BottomNavigationBarItem(icon: Icon(Icons.search), title: Text("Search")),
+          BottomNavigationBarItem(icon: Icon(Icons.trip_origin), title: Text("Trip")),
+          BottomNavigationBarItem(icon: Icon(Icons.event), title: Text("Events")),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,43 @@
-//import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'models/global.dart';
-import './addtrip.dart';
-//import 'package:trie/trie.dart';
+import 'package:expensetracker/HomePage.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    //Creates a whole app ecosystem, All classes written below loads to material app for main
+    home: SplashIntro(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SplashIntro extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Trackenser',
-      theme: ThemeData(
-          //primarySwatch: Colors.cyan,
-          fontFamily: 'Raleway'
-      ),
-      home: MyHomePage(title: 'Expense Tracker'),
+  _SplashIntroState createState() => _SplashIntroState();
+}
+
+class _SplashIntroState extends State<SplashIntro> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyApp(),
+          ),
+        );
+      },
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedTabIndex = 0;
-  int _onSaveIndex = 0;
-  var trips = ['dummy'];
-
-  List _pages = [
-    Text("Home"),
-    //Text("Search"),
-    Text("Trip"),
-    Text("Trip Event Details"),
-  ];
-
-  _changeIndex(int index) {
-    setState(() {
-      _selectedTabIndex = index;
-    });
-  
-  }
-
-  void _onSave(String tripText) {
-    setState(() {
-      _onSaveIndex+=1;
-      trips.add(tripText);
-      print(_onSaveIndex);
-      print(trips);
-      Navigator.pop(context);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-
-    //Screen UI assembly
     return Scaffold(
-        appBar: PreferredSize( //add this to actually increase size of top bar and place appbar
-            preferredSize: Size.fromHeight(120.0),
-
-            child: AppBar(title: Text(widget.title, style: TextStyle(fontFamily: 'open_sans'),),
-                          //titleSpacing: ,
-                          centerTitle: true,
-                          backgroundColor: Colors.lime[900],
-                          shape: RoundedRectangleBorder( borderRadius:
-                                                          BorderRadius.vertical(bottom: Radius.circular(30)
-                                                          )
-                                  ),
-
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.search,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // do something`
-                  },
-                )
-              ],),
-    ),
-        //Container(height: 100,color: Colors.cyan,)
-
-
-      body: ListView(
-      children: [
-      for (var i=1; i<=_onSaveIndex;i++) 
-          
-          Card( child: ListTile(
-                leading: Icon(Icons.radio_button_unchecked),
-                title: Text(trips[i]),
-                subtitle: Text(trips[i]),
-                trailing: Icon(Icons.more_vert),
-          ))
-      ]
-          ,)
-      ,
-      //Center(child: _pages[_selectedTabIndex],),
-
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Addtrip(_onSave),
-          ),);
-        }, //
-        backgroundColor: Colors.lime[800],
-        splashColor: Colors.lime,
-        //tooltip: 'Increment',
-        child: new Icon(Icons.add),
+        body: Center(
+      child: FlutterLogo(
+        size: 200,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedTabIndex,
-        onTap: _changeIndex,
-        selectedItemColor: Colors.blue,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.radio), title: Text("Home")),
-          //BottomNavigationBarItem(icon: Icon(Icons.search), title: Text("Search")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.trip_origin), title: Text("Trip")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.event), title: Text("Events")),
-        ],
-      ),
-    );
+    ));
   }
 }
